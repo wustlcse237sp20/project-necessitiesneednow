4ff9534783fa9632e6d53f46af7f3151e3f9d5f1
Shane Warga <shanewarga@wustl.edu>
2020-04-04 12:37:07 -0500

    utility class to turn hashmap keys and values to arrays

---
+++ necessitiesneedednow/src/main/CSVArrayUtils.java
@@ -0,0 +1,55 @@
+import java.util.Collection;
+import java.util.Set;
+
+public class CSVArrayUtils {
+
+    public String[] itemsToStringArray(Set<String> items){
+        int index = 0;
+        String[] result = new String[items.size()];
+        for (String item: items) {
+            result[index++] = item;
+        }
+        return result;
+    }
+
+    public String[] LocationsToStringArray(Collection<String> locations){
+        int index = 0;
+        String[] result = new String[locations.size()];
+        for (String location: locations) {
+            result[index++] = location;
+        }
+        return result;
+    }
+
+    public Integer[] quantityToIntegerArray(Collection<Integer> quantities){
+        int index = 0;
+        Integer[] result = new Integer[quantities.size()];
+        for (Integer quantity: quantities) {
+            result[index++] = quantity;
+        }
+        return result;
+    }
+
+    public Double[] pricesToDoubleArray(Collection<Double> prices){
+        int index = 0;
+        Double[] result = new Double[prices.size()];
+        for (Double price: prices) {
+            result[index++] = price;
+        }
+        return result;
+    }
+
+    public Boolean[] subscriptionsToBooleanArray(Collection<Boolean> subscriptions){
+        int index = 0;
+        Boolean[] result = new Boolean[subscriptions.size()];
+        for (Boolean status: subscriptions) {
+            result[index++] = status;
+        }
+        return result;
+    }
+
+
+
+
+
+}
