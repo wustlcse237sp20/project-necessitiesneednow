17243ae42ee78669bd98ac5c37d109a6ddc14dd8
Shane Warga <shanewarga@wustl.edu>
2020-04-14 15:39:56 -0500

    Writing to CSV needs to handle IOException in the future

--- necessitiesneedednow/src/main/CSVUtils.java
+++ necessitiesneedednow/src/main/CSVUtils.java
@@ -1,23 +1,62 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CSVUtils {
 
     String filePath;
+    String fileName;
 
-
-    public String getFileName() {
-        return filePath;
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
     }
 
     public void setFilePath(String filePath) {
         this.filePath = filePath;
     }
 
+    public String getFileName() {
+        return fileName;
+    }
 
-    private ArrayList<String[]> shoppingCartToStringArray(shoppingList cart){
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void createTestCSV(ArrayList<String[]> rows) throws IOException {
+        File csv = new File(filePath+ "\\" +fileName);
+        FileWriter writer = new FileWriter(csv);
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+        for (String[] row : rows) {
+            bufferedWriter.append(String.join(",",row));
+            bufferedWriter.append("\n");
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        }
+    }
+
+    public Boolean createCSV(shoppingList cart) throws IOException {
+        File csv = new File(filePath+ "\\" +fileName);
+        ArrayList<String> added = new ArrayList<String>(cart.totalAmountOfItem.keySet().size());
+        ArrayList<String[]> rows = shoppingCartToStringArray(cart);
+        FileWriter writer = new FileWriter(csv);
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+        for (String[] row : rows) {
+            bufferedWriter.append(String.join(",",row));
+            bufferedWriter.append("\n");
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        }
+        return true;
+    }
+
+    /*java doc here for the method below*/
+    public ArrayList<String[]> shoppingCartToStringArray(shoppingList cart){
         CSVArrayUtils util = new CSVArrayUtils();
         int size = cart.totalAmountOfItem.keySet().size();
         ArrayList<String[]> rows = new ArrayList<String[]>(size);
@@ -29,8 +68,8 @@ public class CSVUtils {
         String[] row = new String[5];
         for(int index = 0; index < size; index++){
             row[0] = items[index];
-            row[1] = locations[index];
-            row[2] = quantities[index].toString();
+            row[1] = quantities[index].toString();
+            row[2] = locations[index];
             row[3] = prices[index].toString();
             row[4] = subscriptions[index].toString();
             rows.add(row);
@@ -38,31 +77,35 @@ public class CSVUtils {
         return rows;
     }
 
-
-    public Boolean createCSV(shoppingList cart) {
-        File csv = new File(filePath);
-        ArrayList<String[]> rows = shoppingCartToStringArray(cart);
-        try (PrintWriter writer = new PrintWriter(csv)) {
-            rows.stream().map(this::formatToCSV).forEach(writer::println);
-            return true;
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-            return false;
+    private String removeSpecialCharacters(String data) {
+        String editedString = data.replaceAll("\\R", " ");
+        if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+            data = data.replace("\"", "\"\"");
+            editedString = "\"" + data + "\"";
         }
+        return editedString;
     }
 
-    private String removeSpecialCharacters(String data){
-    String editedString = data.replaceAll("\\R", " ");
-    if(data.contains(",") || data.contains("\"") || data.contains("'")){
-        data = data.replace("\"", "\"\"");
-        editedString = "\"" + data + "\"";
-    }
-    return editedString;
+    private ArrayList<String[]> removeAllSpecialCharacters(ArrayList<String[]> array){
+        ArrayList<String[]> result = new ArrayList<>(array.size());
+        for(int arrayIndex = 0; arrayIndex < array.size(); arrayIndex++){
+            for(int stringArrayIndex = 0; stringArrayIndex < array.get(arrayIndex).length; stringArrayIndex++) {
+                String wordToChange = array.get(arrayIndex)[stringArrayIndex];
+                String editedWord = removeSpecialCharacters(wordToChange);
+                array.get(arrayIndex)[stringArrayIndex] = editedWord;
+            }
+        }
+        return result;
     }
 
-    private String formatToCSV(String[] data){
-        return Stream.of(data).map(this::removeSpecialCharacters).collect(Collectors.joining(","));
+  /* private String formatToCSV(String[] data){
+        StringJoiner joiner = new StringJoiner(",");
+        for (String datum : data) {
+            String s = removeSpecialCharacters(datum);
+            joiner.add(s);
+        }
+        return joiner.toString();
     }
-
+*/
 
 }
---
+++ necessitiesneedednow/src/main/CSVUtilsTest.java
@@ -0,0 +1,59 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class CSVUtilsTest {
+
+    private shoppingList listForTesting = new shoppingList("04-13-20");
+    private CSVUtils csvUtils = new CSVUtils();
+
+
+    @Test
+    void setFilePath() {
+        csvUtils.setFilePath("C:\\Users\\Shane Warga\\Desktop");
+        String expected = "C:\\Users\\Shane Warga\\Desktop";
+        Assert.assertEquals(expected,csvUtils.getFilePath());
+    }
+
+    @Test
+    void setFileName(){
+        csvUtils.setFileName("date.csv");
+        String expected = "date.csv";
+        Assert.assertEquals(expected,csvUtils.getFileName());
+    }
+
+    @Test
+    void shoppingCartToStringArray(){
+        listForTesting.addItem("Apple", 3, "Apple Store", 1000.00, true);
+        ArrayList<String[]> result = csvUtils.shoppingCartToStringArray(listForTesting);
+        ArrayList<String[]> expected = new ArrayList<String[]>();
+        int three = 3;
+        double price = 1000.00;
+        boolean bool = true;
+        String[] item = {"Apple",Integer.toString(three),"Apple Store",Double.toString(price), Boolean.toString(bool)};
+        expected.add(item);
+        Assert.assertEquals(expected.get(0), result.get(0));
+    }
+
+    @Test
+    void createTestCSV() throws IOException {
+        csvUtils.setFilePath("E:\\NEEDNESSNOW");
+        csvUtils.setFileName("test.csv");
+        listForTesting.addItem("Apple", 3, "Apple Store", 1000.00, true);
+        csvUtils.createTestCSV(csvUtils.shoppingCartToStringArray(listForTesting));
+    }
+
+
+    @Test
+    void createCSV() throws IOException {
+        csvUtils.setFilePath("E:\\NEEDNESSNOW");
+        csvUtils.setFileName("date.csv");
+        listForTesting.addItem("Apple", 3, "Apple Store", 1000.00, true);
+        boolean returned = csvUtils.createCSV(listForTesting);
+        Assert.assertEquals(true, returned);
+    }
+}
