1b976927b6ccd38e1fd591bfc82fcb08b8201e32
Steven Lee <lee.steven@wustl.edu>
2020-05-01 00:12:27 -0700

    minor fixes

--- necessitiesneedednow/src/main/App.java
+++ necessitiesneedednow/src/main/App.java
@@ -3,22 +3,8 @@ import javax.swing.*;
 public class App {
 
     public static void main(String[] args) {
-
         JFrame mainShoppingFrame = new mainShopping("NNN Shopping");
         mainShoppingFrame.setResizable(false);
         mainShoppingFrame.setVisible(true);
-
-//        Item Apples = new Item ("Apples", 234, 234, true);
-//        Item Pears = new Item ("Pears", 234,234 , false );
-//        Item Carrots = new Item ("Carrots", 234, 234, false);
-//        Item Peas = new Item ("Peas", 234, 234, false);
-//        shoppingList testList = new shoppingList("222");
-//        testList.addItem(Apples,2,"Walmart", false);
-//        testList.addItem(Pears, 2, "easdf", false);
-//        testList.addItem(Carrots,2,"asdf",true);
-//        testList.addItem(Peas, 2, "asdf", true);
-
-//        JFrame shoppingListFrame = new shoppingListDisplayGUI("Your Current Shopping List", testList);
-//        shoppingListFrame.setVisible(true);
     }
 }
