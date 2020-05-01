ab1910ce09e4e9fb3b644b032abdd8ae77ff2ff4
Steven Lee <lee.steven@wustl.edu>
2020-05-01 00:18:51 -0700

    Deleted extraneous files

--- out/production/project-necessitiesneednow/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd main
-javac -cp main/json-20190722.jar org.json/JsonParser.java
-javac CSVArrayUtils.java CSVUtils.java shoppingList.java testing.java User.java ShoppingAPI.java
-java testing
